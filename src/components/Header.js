5ce7f8187d080e1d44189b7f6379e0cc045c1c7c
Croip3 <croip3@gmail.com>
2021-05-29 22:56:18 +0200

    first commit, added header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../css/header.css'
+
+const header = () => {
+    return (
+        <header>
+            <h1>Hi hi</h1>
+        </header>
+    )
+}
+
+export default header
